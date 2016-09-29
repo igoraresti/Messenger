91d8d28e6caae998858ff854767cdf32257eb8a8
Igor <sibori90@hotmail.com>
2016-09-29 14:05:35 +0200

    Renombrados los métodos para test

--- src/test/java/org/iaresti/messenger/database/DatabaseTest.java
+++ src/test/java/org/iaresti/messenger/database/DatabaseTest.java
@@ -1,15 +1,13 @@
 package org.iaresti.messenger.database;
 
+import static org.junit.Assert.*;
+
 import java.util.Calendar;
 import java.util.List;
-import java.util.UUID;
 
 import org.iaresti.messenger.model.Message;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
-import junit.framework.Assert;
 
 public class DatabaseTest {
 
@@ -19,44 +17,44 @@ public class DatabaseTest {
     }
 
     @Test
-    public void databaseGetMessageTest() {
+    public void should_be_able_to_get_single_message() {
         Message messageDatabase = Database.getInstance().getMessage("1");
-        Assert.assertEquals("Hola 1", messageDatabase.getMessage());
+        assertEquals("Hola 1", messageDatabase.getMessage());
     }
 
     @Test
-    public void databaseGetMessageForYearTest() {
+    public void should_be_able_to_get_message_by_year() {
         List<Message> messagesDatabase = Database.getInstance().getMessagesForYear(Calendar.YEAR);
         Calendar calendar = Calendar.getInstance();
 
         for (Message message : messagesDatabase) {
             calendar.setTime(message.getCreated());
-            Assert.assertEquals(Calendar.YEAR, calendar.get(Calendar.YEAR));
+            assertEquals(Calendar.YEAR, calendar.get(Calendar.YEAR));
         }
     }
 
     @Test
-    public void databaseAddMessageTest() {
+    public void should_be_able_to_add_message() {
         Message message = new Message("Prueba", "Moe");
         Database database = Database.getInstance();
         database.addMessage(message);
-        Assert.assertEquals(message, database.getMessage(message.getId()));
+        assertEquals(message, database.getMessage(message.getId()));
     }
 
     @Test
-    public void databaseUpdateMessage() {
+    public void should_be_able_to_update_message() {
         Database database = Database.getInstance();
-        Assert.assertEquals(null, database.getMessage("-1"));
+        assertEquals(null, database.getMessage("-1"));
         Message message = new Message("1", "Adios 1", "Igor");
         database.updateMessage(message);
-        Assert.assertEquals("Adios 1", database.getMessage("1").getMessage());
+        assertEquals("Adios 1", database.getMessage("1").getMessage());
     }
 
     @Test
-    public void removeMessage() {
+    public void should_be_able_to_remove_message() {
         Database database = Database.getInstance();
         database.removeMessage("1");
-        Assert.assertEquals(null, database.getMessage("1"));
+        assertEquals(null, database.getMessage("1"));
     }
 
 }
